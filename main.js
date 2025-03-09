6d3831a06b05f5c3240a197501d7ddc6d1c3cc70
Balearica <admin@scribeocr.com>
2025-03-09 13:46:38 -0700

    Fixed bugs in previous commits to use latest version of scribe.js

--- main.js
+++ main.js
@@ -270,6 +270,12 @@ elem.edit.styleSuper.addEventListener('click', () => {
   });
 });
 
+elem.edit.styleUnderline.addEventListener('click', () => {
+  ScribeViewer.CanvasSelection.modifySelectedWordStyle({
+    underline: elem.edit.styleUnderline.checked,
+  });
+});
+
 elem.edit.wordFont.addEventListener('change', () => {
   ScribeViewer.CanvasSelection.modifySelectedWordStyle({
     font: elem.edit.wordFont.value,
@@ -347,7 +353,7 @@ export function adjustBaselineRange(value) {
   // This allows the properties to be accurate if the user ever switches the word to non-superscripted.
   objectsLine.forEach((objectI) => {
     objectI.topBaseline = objectI.topBaselineOrig + (valueNum - baselineRange);
-    if (!objectI.word.sup) {
+    if (!objectI.word.style.sup) {
       objectI.yActual = objectI.topBaseline;
     }
   });
@@ -389,6 +395,7 @@ export function toggleEditButtons(disable = true) {
   elem.edit.styleBold.disabled = disable;
   elem.edit.styleSmallCaps.disabled = disable;
   elem.edit.styleSuper.disabled = disable;
+  elem.edit.styleUnderline.disabled = disable;
 
   elem.edit.deleteWord.disabled = disable;
   elem.edit.recognizeWord.disabled = disable;
@@ -769,11 +776,6 @@ async function clearUI() {
 
 clearFiles();
 
-const styleItalicButton = new Button(elem.edit.styleItalic);
-const styleBoldButton = new Button(elem.edit.styleBold);
-const styleSmallCapsButton = new Button(elem.edit.styleSmallCaps);
-const styleSuperButton = new Button(elem.edit.styleSuper);
-
 ScribeViewer.KonvaOcrWord.updateUI = () => {
   const wordFirst = ScribeViewer.CanvasSelection.getKonvaWords()[0];
 
@@ -793,20 +795,11 @@ ScribeViewer.KonvaOcrWord.updateUI = () => {
     elem.edit.fontSize.value = '';
   }
 
-  if (wordFirst.word.sup !== elem.edit.styleSuper.classList.contains('active')) {
-    styleSuperButton.toggle();
-  }
-  if (wordFirst.word.smallCaps !== elem.edit.styleSmallCaps.classList.contains('active')) {
-    styleSmallCapsButton.toggle();
-  }
-  const italic = wordFirst.word.style === 'italic';
-  if (italic !== elem.edit.styleItalic.classList.contains('active')) {
-    styleItalicButton.toggle();
-  }
-  const bold = wordFirst.word.style === 'bold';
-  if (bold !== elem.edit.styleBold.classList.contains('active')) {
-    styleBoldButton.toggle();
-  }
+  elem.edit.styleSuper.checked = wordFirst.word.style.sup;
+  elem.edit.styleSmallCaps.checked = wordFirst.word.style.smallCaps;
+  elem.edit.styleUnderline.checked = wordFirst.word.style.underline;
+  elem.edit.styleItalic.checked = wordFirst.word.style.italic;
+  elem.edit.styleBold.checked = wordFirst.word.style.bold;
 };
 
 ScribeViewer.KonvaLayout.updateUI = () => {
